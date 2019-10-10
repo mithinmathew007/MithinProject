56f1c1ba2bc1054ef6bab69d994d7e41f5a5485b
mithinmathew007 <56343449+mithinmathew007@users.noreply.github.com>
2019-10-10 14:16:37 +0530

    test

--- src/test/java/org/ravi/helloworld/HelloTest.java
+++ src/test/java/org/ravi/helloworld/HelloTest.java
@@ -15,12 +15,12 @@ import org.testng.annotations.AfterTest;
 
 public class HelloTest {
 
-	WebDriver driver;
+	/*WebDriver driver;
 	WebElement text;
 	@BeforeTest
 	public void beforeTest() {
 		/*	System.setProperty("webdriver.gecko.driver", "resources\\geckodriver32.exe");
-		driver = new FirefoxDriver();*/
+		driver = new FirefoxDriver();
 		System.setProperty("webdriver.chrome.driver","resources\\chromedriver.exe");
 		driver = new ChromeDriver();
 		//driver.get("http://localhost:8080/Helloworld.war/");
@@ -39,7 +39,7 @@ public class HelloTest {
 	@AfterTest
 	public void afterTest() {
 		//driver.close();
-	}
+	}*/
 
 
 }
